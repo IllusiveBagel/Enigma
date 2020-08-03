da8749741108323d8e75e91188a39d29a2581ee9
IllusiveBagel <hawkentroop@gmail.com>
2020-08-03 12:38:44 +0100

    Added settings similar to a settings sheet

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.Extensions.Configuration;
+
+namespace Enigma
+{
+    class Program
+    {
+        public static IConfigurationRoot configuration;
+
+        static void Main(string[] args)
+        {
+            // Add Configuration File
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            Console.WriteLine(configuration.GetSection("test").Value);
+        }
+    }
+}
